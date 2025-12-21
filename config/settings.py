921a12421e3dd6545e2fc83aaa5d5e7b06535258
parv <parv@rhour.com>
2025-12-21 21:03:30 +0530

    Async deploy 2

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.postgres'
+    'django.contrib.postgres',
     'pgvector',
     'apps.core',
     'apps.ingestion',
