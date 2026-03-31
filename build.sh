456a41a008ec64fb80e0472f7f7bce98e47626fe
parv <parv@rhour.com>
2026-04-01 01:13:45 +0530

    Staging commit - Langchain & unit tests 1.1

--- build.sh
+++ build.sh
@@ -5,11 +5,6 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Running Unit Tests..."
-
-# If this fails, the script exits immediately and deployment is aborted
-python manage.py test
-
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
