06d0ce20328a538d4d849b608073e22b6b0fd6c5
parv <parv@rhour.com>
2026-04-06 17:52:18 +0530

    Added interval for celery polling + removed worker startup script

--- render_start.sh
+++ render_start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Starting Celery Worker..."
-celery -A config worker --loglevel=info --concurrency=2 &
+celery -A config worker --loglevel=info --concurrency=4 &
 
 echo "Starting Web Server..."
 uvicorn config.asgi:application --host 0.0.0.0 --port $PORT
