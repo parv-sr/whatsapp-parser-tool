e249354d0f89c386c56efc121c1acbead4f19ec0
parv <parv@rhour.com>
2026-04-07 21:20:15 +0530

    Rust parser setup 2

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ set -o errexit
 echo "Installing dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
+cargo build --release
+
+cd rust_parser/whatsapp-parser && cargo build --release && \
+cd /app && mkdir -p /app/bin && \
+cp rust_parser/whatsapp-parser/target/release/whatsapp-parser /app/bin/whatsapp-parser && \
+chmod +x /app/bin/whatsapp-parser
+
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
